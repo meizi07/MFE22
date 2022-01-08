9fdf41f67443c2adb5995d3ad0bb66383995aa86
meizi07 <rainy2525777@gmail.com>
2022-01-08 15:41:07 +0800

    add promise1.js

---
+++ callback/promise1.js
@@ -0,0 +1,59 @@
+// Promise 物件代表一個即將完成、或失敗的非同步操作，以及它所產生的值。
+
+// 1. Promise 是一個物件
+// new Promise()
+// Promise() 是一個建構式，它需要一個參數 executor
+// 這個 executor 也是一個函式 function() 也有兩個參數 resolve reject
+// resolve 式成功的時候要呼叫的
+// reject 是失敗的時候要呼叫
+// 2. 即將完成、或失敗
+// 3. 非同步
+
+// Promise 物件會有狀態移轉
+// 剛建立的時候狀態會是 pending
+
+
+// new Promise((resolve, reject) => {});
+
+
+let doWork = function (job, timer) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`完成工作 ${job}`); // 會把這一個 promise 物件的狀態變成 fulfilled
+
+            // 如果發生錯誤
+            // reject(err)
+            // 會把這一個 promise 物件的狀態變成 rejected
+        }, timer);
+    });
+};
+
+// 刷牙 --> 吃早餐 --> 寫功課
+let dt = new Date();
+console.log(`Start ${dt.toISOString()}`);
+// let work1Promise = doWork("刷牙", 2000);
+// work1Promise.then((result) => {
+//   let dt = new Date();
+//   console.log(`${result} at ${dt.toISOString()}`);
+// });
+
+doWork("刷牙", 2000)
+    .then((result) => {
+        let dt = new Date();
+        console.log(`${result} at ${dt.toISOString()}`);
+
+        return doWork("吃早餐", 3000);
+    })
+    .then((result) => {
+        let dt = new Date();
+        console.log(`${result} at ${dt.toISOString()}`);
+        return doWork("寫功課", 2000);
+    })
+    .then((result) => {
+        let dt = new Date();
+        console.log(`${result} at ${dt.toISOString()}`);
+    })
+    .catch((err) => {
+        // 處理錯誤
+        console.error(err);
+    });
