4d35fa92f2ca128afaf6cf8b6e7bd4d1c60cf132
meizi07 <rainy2525777@gmail.com>
2022-01-09 16:51:19 +0800

    add crawler

---
+++ crawler/crawler.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220109&stockNo=2330&_=1641717221134")
+    .then(function (response) {
+        // handle success
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    });
