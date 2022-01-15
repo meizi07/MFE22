1c257d04885d9ef5954ae28fe29c8ea3e3e508ad
meizi07 <rainy2525777@gmail.com>
2022-01-15 12:26:03 +0800

    add crawler3.js

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,17 +1,17 @@
-const axios = require("axios");
+const axios = require('axios');
+const { readFile } = require('fs/promises');
+
 
 (async () => {
     try {
-        // 根據變數去抓取資料
-        let stockNo = 2330;
-        let queryDate = "20220115";
-
-        // let response = await axios.get(
-        //   `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${queryDate}&stockNo=${stockNo}`
-        // );
+        // TODO: 從stock.txt中讀出檔案代碼
+        let stockNo = await readFile('stock.txt', 'utf-8');
+        console.log(stockNo);
+        let queryDate = "20220110"; //TODO: 自動用今天的日期
 
         let response = await axios.get(
-            "https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+            "https://www.twse.com.tw/exchangeReport/STOCK_DAY", 
+            {
                 // 這裡可以放一些設定
                 // params: 放 query string 的參數
                 params: {
@@ -21,7 +21,6 @@ const axios = require("axios");
                 },
             }
         );
-
         console.log(response.data);
     } catch (e) {
         console.error(e);
